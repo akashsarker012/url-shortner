8802edd7ee6b3f1933da6ab53d637a7a439af284
Akash Sarker <akashsarker.dev@gmail.com>
2024-10-13 20:05:50 +0600

    backend setup and schema create

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
